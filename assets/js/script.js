bab2292046d160542fe8dca62207b1308bc7d926
Arun Kumar Vr <arun.vr@electronikmedia.com>
2023-10-26 00:23:32 +0530

    list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 (function($) {
   $(document).ready(function() {
 
-/*
+
   $('body a').each(function(){ 
     var oldUrl = $(this).attr("href"); 
     var newUrl = ("/ordermeal" + oldUrl );
     $(this).attr("href", newUrl);
   });
-*/
+
 
     $(window).scroll(function(){
         if ($(window).scrollTop() >= 250) {
@@ -18,8 +18,6 @@
         }
     });
 
-
-    
   $(document.body).on('click', '.show_filter' ,function(){
     if ($('body').hasClass('active_filter')) {
       $('body').removeClass("active_filter");
@@ -27,38 +25,6 @@
       $('body').addClass("active_filter");
     }
   });
-    
-
-
-  var swiper = new Swiper(".meal-slider", {
-    spaceBetween: 0,
-    slidesPerView: 1,
-    effect: "fade",
-    speed: 3000,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false
-    },
-
-  });
-
-
-
-  $(".count_input span").on("click", function() {
-    var $button = $(this);
-    var oldValue = parseInt($button.parent().find("input").val());
-    var newVal;
-
-    if ($button.text() === "+") {
-      newVal = oldValue + 1;
-    } else {
-      // Don't allow decrementing below zero
-      newVal = (oldValue > 1) ? oldValue - 1 : 1;
-    }
-    $button.parent().find("input").val(newVal);
-  });
-
-
 
 
 
